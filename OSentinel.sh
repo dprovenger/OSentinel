7bc94bd408039ca6a1c2cda43425c633466a9f61
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2021-04-25 01:25:56 -0500

    Adding systemd enabled services output

--- OSentinel.sh
+++ OSentinel.sh
@@ -13,6 +13,10 @@ HOSTNAME=`hostname`
 # CentOS release checking variable
 COSRELEASE=`cat /etc/centos-release 2>/dev/null || echo "Not a CentOS system"`
 
+# Systemd enabled services variable
+SERVICES=`systemctl list-unit-files 2>/dev/null | grep enabled || echo "Not a systemd system"`
+
 # Adding initial output
-echo $LINE; echo "OS info:"
-echo $LINE; echo "Hostname = $HOSTNAME"; echo $LINE; echo "CentOS Release = $COSRELEASE"
+echo $LINE; echo "OS info:"; echo $LINE
+echo "Hostname = $HOSTNAME"; echo $LINE; echo "CentOS Release = $COSRELEASE"; echo $LINE
+echo "Systemctl enabled services :"; echo "$SERVICES"; echo $LINE
