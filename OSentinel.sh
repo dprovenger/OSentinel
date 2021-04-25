3bbf0b749dd1504621a7fb11e67dce703c67b8f7
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2021-04-25 01:19:54 -0500

    Adding hostname output

--- OSentinel.sh
+++ OSentinel.sh
@@ -5,8 +5,9 @@
 #
 # Variable to separate output
 LINE="----------"
-
+# Hostname Variable
+HOSTNAME=`hostname`
 
 # Adding initial output
 echo $LINE; echo "OS info:"
-echo $LINE
+echo $LINE; echo "Hostname = $HOSTNAME"
