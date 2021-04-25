201f6047a55a7420c57a12f06c7120fe5744fd63
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2021-04-25 01:22:39 -0500

    Adding CentOS checking output

--- OSentinel.sh
+++ OSentinel.sh
@@ -3,11 +3,16 @@
 # dprovenger
 # https://github.com/dprovenger/OSentinel
 #
+
 # Variable to separate output
 LINE="----------"
+
 # Hostname Variable
 HOSTNAME=`hostname`
 
+# CentOS release checking variable
+COSRELEASE=`cat /etc/centos-release 2>/dev/null || echo "Not a CentOS system"`
+
 # Adding initial output
 echo $LINE; echo "OS info:"
-echo $LINE; echo "Hostname = $HOSTNAME"
+echo $LINE; echo "Hostname = $HOSTNAME"; echo $LINE; echo "CentOS Release = $COSRELEASE"
